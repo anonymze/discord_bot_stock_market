d2a18bbe16ee83ed2ade37379a56a6017eb08925
yann <metier.game@gmail.com>
2022-07-02 14:10:50 +0200

    type event

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token, guildId } = require('./config.json');
 const notificationLimitOrder = require('./cron/notification.js');
 
 const handleCommand = require('./helpers/command');
 const handleSelectMenu = require('./helpers/select-menu');
 
+// Intents (what bot can receive from events)
+const myIntents = new Intents(32767);
+// myIntents.add(Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES);
+
 // client discord
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: myIntents });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -24,6 +28,26 @@ client.once('ready', function () {
 client.on('interactionCreate', async interaction => {
     if (interaction.isCommand()) handleCommand(client, interaction);
     if (interaction.isSelectMenu()) handleSelectMenu(interaction);
+});
+
+client.on('userUpdate', async (oldMember, newMember) => {
+    console.log('2');
+    console.log(oldMember, newMember);
+});
+
+client.on('typingStart', async (typingEvent) => {
+    const {user, channel, startedTimestamp} = typingEvent;
+    let lastTypingTimestamp;
+
+    // every 15 min
+    let betweenSchedule = lastTypingTimestamp ? new Date(lastTypingTimestamp + (1 * 60000)).getTime() : startedTimestamp;
+
+    // channel général ID + flavio ID
+    if (startedTimestamp >= betweenSchedule) {
+    // if (channel.id === '767805471951421452' && user.id === '269781481285615616' && startedTimestamp > betweenSchedule) {
+        lastTypingTimestamp = startedTimestamp;
+        console.log('flavio écrit');
+    }  
 });
 
 client.login(token);
