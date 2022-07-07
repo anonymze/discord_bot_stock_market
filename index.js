669f558d51f6c40c66b0358a2e545f7b7e771676
yann <metier.game@gmail.com>
2022-07-07 17:53:16 +0200

    fix avatar(

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ client.on('userUpdate', async (oldMember, newMember) => {
         const embededMessage = new MessageEmbed();
         embededMessage.setTitle(texts[Math.floor(Math.random() * texts.length)])
             .setColor('#2aa198')
+            .setDescription('Nouvelle pp du grand maître')
             .setImage(user.displayAvatarURL());
 
         channel.send({ embeds: [embededMessage] });
